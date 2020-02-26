11744681bec7c31e1c0db82a12c1942308801c7e
Kiran Ramesh <kiranramesh135@gmail.com>
2020-02-26 05:48:07 +0530

    Reformat code

--- src/main/java/com/restassuredautomation/base/BaseTestNG.java
+++ src/main/java/com/restassuredautomation/base/BaseTestNG.java
@@ -1,38 +1,34 @@
 package com.restassuredautomation.base;
 
 
+import org.apache.commons.io.FileUtils;
+import org.testng.annotations.AfterSuite;
+
 import java.io.File;
 import java.io.IOException;
 
-import org.apache.commons.io.FileUtils;
-import org.testng.annotations.AfterSuite;
-import org.testng.annotations.Test;
-
 
 public class BaseTestNG {
-	
-	@AfterSuite
-	public void copyReport() {
-		
-		try {
-		System.out.println(System.getProperty("user.dir"));
-		String path = System.getProperty("user.dir");
-		String reportPath = path +File.separator+"target"+File.separator+"surefire-reports"+File.separator+"ExtentReportsTestNG.html";
-		
-		File srcFile = new File(reportPath);
-		if(srcFile.exists()) {
-			String destResult = path+File.separator+"results"+File.separator+"ExtentReportsTestNG"+System.currentTimeMillis()+".html";
+
+    @AfterSuite
+    public void copyReport() {
+
+        try {
+            System.out.println(System.getProperty("user.dir"));
+            String path = System.getProperty("user.dir");
+            String reportPath = path + File.separator + "target" + File.separator + "surefire-reports" + File.separator + "ExtentReportsTestNG.html";
+
+            File srcFile = new File(reportPath);
+            if (srcFile.exists()) {
+                String destResult = path + File.separator + "results" + File.separator + "ExtentReportsTestNG" + System.currentTimeMillis() + ".html";
 //			System.out.println(a);
-			FileUtils.copyFile(srcFile, new File(destResult));
-		}
-	
-		}
-		catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-	
-	
+                FileUtils.copyFile(srcFile, new File(destResult));
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 
 }
--- src/main/java/com/restassuredautomation/base/Basics3.java
+++ src/main/java/com/restassuredautomation/base/Basics3.java
@@ -1,83 +1,78 @@
 package com.restassuredautomation.base;
 
 
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.equalTo;
-
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.util.Properties;
-
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
-
 import files.PayLoad;
 import files.Resources;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
-public class Basics3 extends BaseTestNG{
-	
-	private static Logger log = LogManager.getLogger(Basics3.class.getName());
-	
-	Properties prop = new Properties();
-	
-	@BeforeMethod
-	public void preCondition() throws IOException {
-		
-		String userDir = System.getProperty("user.dir");
-		
-		
-		FileInputStream fis = new FileInputStream(userDir+"/src/main/resources/conf/env.properties");
-		prop.load(fis);
-	}
-	
-	@Test
-	public void addAndDeletePlace() {
-		
-		//Task 1 - grab the response
-		log.info("Host information"+prop.getProperty("HOST"));
-		RestAssured.baseURI = prop.getProperty("HOST");
-		Response res = given().
-								queryParam("key", prop.getProperty("KEY")).
-								body(PayLoad.getPostData()).
-					   when().
-					   			post(Resources.placePostData()).
-					   then().
-					   			assertThat().statusCode(200).and().contentType(ContentType.JSON).and().
-					   			body("status",equalTo("OK")).
-					   			extract().response();
-		
-		//Task 2 - Grab the place ID from response
-		
-		String responseString = res.asString();
-		log.info(responseString);
-		JsonPath js = new JsonPath(responseString);
-		String placeId = js.get("place_id");
-		log.info("Place ID = " +placeId);
-		
-		//Task 3 - Place this place ID in the Delete request
-		
-		given().
-				queryParam("key", prop.getProperty("KEY")).
-				body("{"+
-				"\"place_id\": \""+placeId+"\""+
-				"}").
-		when().
-			post("/maps/api/place/delete/json").
-		then().
-			assertThat().statusCode(200).and().contentType(ContentType.JSON).and().
-			body("status", equalTo("OK"));
-		
-		
-		
-		
-	//Code is still incomplete
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 
-	}
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class Basics3 extends BaseTestNG {
+
+    private static Logger log = LogManager.getLogger(Basics3.class.getName());
+
+    Properties prop = new Properties();
+
+    @BeforeMethod
+    public void preCondition() throws IOException {
+
+        String userDir = System.getProperty("user.dir");
+        FileInputStream fis = new FileInputStream(userDir + "/src/main/resources/conf/env.properties");
+        prop.load(fis);
+    }
+
+    @Test
+    public void addAndDeletePlace() {
+
+        //Task 1 - grab the response
+        log.info("Host information" + prop.getProperty("HOST"));
+        RestAssured.baseURI = prop.getProperty("HOST");
+        Response res = given().
+                queryParam("key", prop.getProperty("KEY")).
+                body(PayLoad.getPostData()).
+                when().
+                post(Resources.placePostData()).
+                then().
+                assertThat().statusCode(200).and().contentType(ContentType.JSON).and().
+                body("status", equalTo("OK")).
+                extract().response();
+
+        //Task 2 - Grab the place ID from response
+
+        String responseString = res.asString();
+        log.info(responseString);
+        JsonPath js = new JsonPath(responseString);
+        String placeId = js.get("place_id");
+        log.info("Place ID = " + placeId);
+
+        //Task 3 - Place this place ID in the Delete request
+
+        given().
+                queryParam("key", prop.getProperty("KEY")).
+                body("{" +
+                        "\"place_id\": \"" + placeId + "\"" +
+                        "}").
+                when().
+                post("/maps/api/place/delete/json").
+                then().
+                assertThat().statusCode(200).and().contentType(ContentType.JSON).and().
+                body("status", equalTo("OK"));
+
+
+        //Code is still incomplete
+
+    }
 
 }
